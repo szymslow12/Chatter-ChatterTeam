4286269a243405abfdabb908bc7b9b897759fe13
szymslow12 <szym.slow12@gmail.com>
2019-02-05 13:31:21 +0100

    add Client with all fields/methods from uml, run is marked TODO

---
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/Client.java
@@ -0,0 +1,23 @@
+package com.codecool.chatter.controller;
+
+import java.net.Socket;
+
+public class Client {
+
+    private Socket socket;
+    private String host;
+    private int port;
+//    private User client;
+//    private AppController appController;
+
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+//        this.appController = new AppController();
+    }
+
+    //TODO
+    public void run() {
+
+    }
+}
