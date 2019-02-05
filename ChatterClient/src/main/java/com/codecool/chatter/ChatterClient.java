38f05eac6263993a21b90cf44017f8dde0523791
szymslow12 <szym.slow12@gmail.com>
2019-02-05 14:23:20 +0100

    change ChatterClient -> extends Application and overrides start()

--- ChatterClient/src/main/java/com/codecool/chatter/ChatterClient.java
+++ ChatterClient/src/main/java/com/codecool/chatter/ChatterClient.java
@@ -1,8 +1,22 @@
 package com.codecool.chatter;
 
-public class ChatterClient {
+import com.codecool.chatter.controller.Client;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChatterClient extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Chatter Client");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Client client = new Client("localhost", 8080);
+        primaryStage.setScene(client.getMainScene());
+        primaryStage.setMaximized(true);
+        primaryStage.setTitle("Chatter Client");
+        primaryStage.show();
+        client.run();
     }
 }
