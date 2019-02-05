f36ddd565de103aa4b36e25a0a38bca1488a5538
szymslow12 <szym.slow12@gmail.com>
2019-02-05 13:52:05 +0100

    add Room with fields from uml,constructor,setters/getters,addUser()

--- ChatterClient/src/main/java/com/codecool/chatter/model/Room.java
+++ ChatterClient/src/main/java/com/codecool/chatter/model/Room.java
@@ -1,4 +1,88 @@
 package com.codecool.chatter.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+
+    private long id;
+    private String name;
+    private int maxCapacity;
+    private List<User> users;
+    private int port;
+    private Chat chat;
+
+
+    public Room(long id, String name, int port) {
+        this.id = id;
+        this.name = name;
+        this.port = port;
+        this.maxCapacity = 20;
+        this.users = new ArrayList<>();
+    }
+
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Chat getChat() {
+        return chat;
+    }
+
+
+    public void setChat(Chat chat) {
+        this.chat = chat;
+    }
+
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+
+    public int getPort() {
+        return port;
+    }
+
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }
