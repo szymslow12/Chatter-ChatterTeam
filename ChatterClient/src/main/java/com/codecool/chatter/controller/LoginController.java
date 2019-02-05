2466cf7ad7274e20572239ff048505a055c1f50b
szymslow12 <szym.slow12@gmail.com>
2019-02-05 22:50:37 +0100

    refactor -> addStageProperties, setPositions

--- ChatterClient/src/main/java/com/codecool/chatter/controller/LoginController.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/LoginController.java
@@ -15,7 +15,7 @@ public class LoginController {
     private LoginView loginView;
 
     public LoginController() {
-        this.loginView = new LoginView();
+        this.loginView = new LoginView(750, 500, 25);
     }
 
 
@@ -38,17 +38,27 @@ public class LoginController {
 
     private void runLoginStage() {
         Stage stage = new Stage();
+        setStageProperties(stage);
+        setPositions(stage);
+        stage.show();
+    }
+
+
+    private void setStageProperties(Stage stage) {
         stage.setAlwaysOnTop(true);
         stage.setScene(new Scene(loginView));
         stage.setTitle("Login");
-        stage.setMinWidth(500d);
-        stage.setMinHeight(500d);
+        stage.setMinWidth(750);
+        stage.setMinHeight(500);
         stage.initStyle(StageStyle.UNDECORATED);
+    }
+
+
+    private void setPositions(Stage stage) {
         Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
         double x = primScreenBounds.getMaxX();
         double y = primScreenBounds.getMaxY();
-        stage.setX((x / 2) - stage.getMinWidth() * 0.75);
+        stage.setX((x / 2) - stage.getMinWidth() * 0.5);
         stage.setY((y / 2) - stage.getMinHeight() * 0.5);
-        stage.show();
     }
 }
