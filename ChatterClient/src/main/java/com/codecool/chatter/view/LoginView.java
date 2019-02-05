e8c9775e82effa440f33a59e65ec631a087a272f
szymslow12 <szym.slow12@gmail.com>
2019-02-06 00:25:24 +0100

    change height of InputField, add buttonView to children

--- ChatterClient/src/main/java/com/codecool/chatter/view/LoginView.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/LoginView.java
@@ -4,19 +4,24 @@ import javafx.geometry.Insets;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
 public class LoginView extends Pane {
 
     private InputField inputField;
-//    private ButtonView buttonView;
+    private ButtonView buttonView;
     private int width;
     private int height;
 
-    public LoginView(int width, int height, int margin) {
+
+
+    public LoginView(int width, int height, int margin, EventHandler<MouseEvent> onClick) {
         super();
         this.width = width;
         this.height = height;
+        this.buttonView = new ButtonView().getButton("Join", onClick);
         createNicknameInputField(margin);
-//        this.buttonView = new ButtonView();
         renderLoginView();
     }
 
@@ -24,13 +29,14 @@ public class LoginView extends Pane {
     private void createNicknameInputField(int margin) {
         Insets margins = new Insets(margin, margin ,margin,margin);
         double width =  this.width * 0.75;
-        double height = width * 0.75;
+        double height = width * 0.25;
         this.inputField = new InputField("Nickname", false, width, height, margins);
     }
 
 
     private void renderLoginView() {
         getChildren().add(inputField);
+        getChildren().add(buttonView);
         setBackground(
             new Background(
                 new BackgroundFill(Color.web("#66ffff"), CornerRadii.EMPTY, Insets.EMPTY)
