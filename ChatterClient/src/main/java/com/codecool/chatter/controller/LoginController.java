01c6a28533d77dbcb99556799b37b15e63ba1043
szymslow12 <szym.slow12@gmail.com>
2019-02-05 20:30:44 +0100

    add runLoginStage and use it in run()

--- ChatterClient/src/main/java/com/codecool/chatter/controller/LoginController.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.codecool.chatter.controller;
 
 import com.codecool.chatter.view.LoginView;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.net.Socket;
 
@@ -22,11 +27,28 @@ public class LoginController {
 
     //TODO
     public void run() {
-
+        runLoginStage();
     }
 
 
     public LoginView getLoginView() {
         return loginView;
+    }
+
+
+    private void runLoginStage() {
+        LoginController loginController = new LoginController();
+        LoginView loginView = loginController.getLoginView();
+        Stage stage = new Stage();
+        stage.setAlwaysOnTop(true);
+        stage.setScene(new Scene(loginView));
+        stage.setTitle("Login");
+        stage.setMinWidth(500d);
+        stage.setMinHeight(500d);
+        stage.initStyle(StageStyle.UNDECORATED);
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setX((primScreenBounds.getWidth() / 2) - stage.getMinWidth() * 0.75);
+        stage.setY((primScreenBounds.getHeight() / 2) - stage.getMinHeight() * 0.5);
+        stage.show();
     }
 }
