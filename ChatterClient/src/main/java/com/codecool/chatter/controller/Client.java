e693885c12fd8ec52ae109e18985ad2c618caa2b
szymslow12 <szym.slow12@gmail.com>
2019-02-05 14:09:59 +0100

    remove fields socket, uncomment AppControllers lines

--- ChatterClient/src/main/java/com/codecool/chatter/controller/Client.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/Client.java
@@ -1,18 +1,15 @@
 package com.codecool.chatter.controller;
 
-import java.net.Socket;
-
 public class Client {
 
-    private Socket socket;
     private String host;
     private int port;
-//    private AppController appController;
+    private AppController appController;
 
     public Client(String host, int port) {
         this.host = host;
         this.port = port;
-//        this.appController = new AppController();
+        this.appController = new AppController();
     }
 
     //TODO
