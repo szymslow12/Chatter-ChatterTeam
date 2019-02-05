0e6c74badeedff0715a8938232ba325c57b1621a
szymslow12 <szym.slow12@gmail.com>
2019-02-05 13:55:37 +0100

    add copies of model from ChatterClient to ChatterServer

---
+++ ChatterServer/src/main/java/com/codecool/chatter/model/Chat.java
@@ -0,0 +1,40 @@
+package com.codecool.chatter.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chat {
+
+    private List<Message> messages;
+    private transient User client;
+
+    public Chat() {
+        this.messages = new ArrayList<>();
+    }
+
+
+    public Chat(User client) {
+        this();
+        this.client = client;
+    }
+
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+
+    public void addMessage(Message message) {
+        messages.add(message);
+    }
+
+
+    public User getClient() {
+        return client;
+    }
+
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+}
