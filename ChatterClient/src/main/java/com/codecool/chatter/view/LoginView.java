df116862c99b28f2f2f360fefe0ed870dc69a9f6
szymslow12 <szym.slow12@gmail.com>
2019-02-05 22:43:00 +0100

    add width, height,sets them in constructor,add createNicknameInputField

--- ChatterClient/src/main/java/com/codecool/chatter/view/LoginView.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/LoginView.java
@@ -6,23 +6,36 @@ import javafx.scene.paint.Color;
 
 public class LoginView extends Pane {
 
-//    private InputField inputField;
+    private InputField inputField;
 //    private ButtonView buttonView;
+    private int width;
+    private int height;
 
-    public LoginView() {
-//        this.inputField = new InputField();
-//        this.buttonView = new ButtonView();
+    public LoginView(int width, int height) {
         super();
+        this.width = width;
+        this.height = height;
+        createNicknameInputField();
+//        this.buttonView = new ButtonView();
         renderLoginView();
     }
 
 
+    private void createNicknameInputField() {
+        Insets margins = new Insets(25, 25 ,25 ,25);
+        double width =  this.width * 0.75;
+        double height = width * 0.75;
+        this.inputField = new InputField("Nickname", false, width, height, margins);
+    }
+
+
     private void renderLoginView() {
+        getChildren().add(inputField);
         setBackground(
             new Background(
                 new BackgroundFill(Color.web("#66ffff"), CornerRadii.EMPTY, Insets.EMPTY)
             )
         );
-        setMinSize(750d, 500d);
+        setMinSize(width, height);
     }
 }
