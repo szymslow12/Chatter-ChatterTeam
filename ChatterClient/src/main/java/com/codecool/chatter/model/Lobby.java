a80a56a04d6b7048bbb1c0e341e5458cdb8ded16
szymslow12 <szym.slow12@gmail.com>
2019-02-05 13:54:53 +0100

    add Lobby with fields and methods from uml and constructor

--- ChatterClient/src/main/java/com/codecool/chatter/model/Lobby.java
+++ ChatterClient/src/main/java/com/codecool/chatter/model/Lobby.java
@@ -1,4 +1,46 @@
 package com.codecool.chatter.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lobby {
+
+    private List<Room> rooms;
+    private transient List<User> users;
+
+
+    public Lobby() {
+        this.rooms = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+
+
+    public void addRoom(Room room) {
+        rooms.add(room);
+    }
+
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
 }
