1e06348290896c51b970597504483dc24032e2e7
szymslow12 <szym.slow12@gmail.com>
2019-02-05 14:32:40 +0100

    add LoginView with fields from uml and marked TODO renderLoginView

---
+++ ChatterClient/src/main/java/com/codecool/chatter/view/LoginView.java
@@ -0,0 +1,21 @@
+package com.codecool.chatter.view;
+
+import javafx.scene.layout.Pane;
+
+public class LoginView extends Pane {
+
+//    private InputField inputField;
+//    private ButtonView buttonView;
+
+    public LoginView() {
+//        this.inputField = new InputField();
+//        this.buttonView = new ButtonView();
+        renderLoginView();
+    }
+
+
+    //TODO
+    public void renderLoginView() {
+
+    }
+}
