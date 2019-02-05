487a5adc1c0e32e11341b1d667e5d803bafdc0db
szymslow12 <szym.slow12@gmail.com>
2019-02-05 14:16:57 +0100

    add import and AppView has setted background

--- ChatterClient/src/main/java/com/codecool/chatter/view/AppView.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/AppView.java
@@ -1,11 +1,20 @@
 package com.codecool.chatter.view;
 
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class AppView extends Pane {
 
     //TODO
     public void renderAppView() {
-
+        setBackground(
+            new Background(
+                new BackgroundFill(Color.web("#66ffff"), CornerRadii.EMPTY, Insets.EMPTY)
+            )
+        );
     }
 }
