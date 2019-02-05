ba6cf85e56737ede6cb6377226ab43cafd8d95f7
szymslow12 <szym.slow12@gmail.com>
2019-02-05 14:08:58 +0100

    add AppController with fields and methods from UML,mark run() TODO

---
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/AppController.java
@@ -0,0 +1,31 @@
+package com.codecool.chatter.controller;
+
+import com.codecool.chatter.model.User;
+
+import java.net.Socket;
+
+public class AppController {
+
+    private Socket connection;
+//    private LobbyController lobbyController;
+//    private RoomController roomController;
+//    private AppView appView;
+    private User client;
+
+    public AppController(){}
+
+
+    public AppController(Socket connection) {
+        this.connection = connection;
+//        this.lobbyController = new LobbyController(connection);
+//        this.roomController = new RoomController(connection);
+    }
+
+
+    //TODO
+    public void run() {
+
+    }
+
+
+}
