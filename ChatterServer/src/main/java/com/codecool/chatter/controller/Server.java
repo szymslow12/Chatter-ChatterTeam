bb6dcb789836e1b1bc36ea2e3a9f0f6c8f148cdd
szymslow12 <szym.slow12@gmail.com>
2019-02-05 13:57:47 +0100

    add Server with fields and methods from uml, marked TODO run()

---
+++ ChatterServer/src/main/java/com/codecool/chatter/controller/Server.java
@@ -0,0 +1,22 @@
+package com.codecool.chatter.controller;
+
+import java.net.ServerSocket;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private String host;
+    private int port;
+//    private AppController appController;
+
+    public Server(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+
+    //TODO
+    public void run() {
+
+    }
+}
