168db12a4e2a40f028bfdab9c3f0958ef90b08a5
szymslow12 <szym.slow12@gmail.com>
2019-02-06 22:20:29 +0100

    add params double width,height and pass it to AppController

--- ChatterClient/src/main/java/com/codecool/chatter/controller/Client.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/Client.java
@@ -7,8 +7,8 @@ public class Client {
     private AppController appController;
     //changes to UML removed field host/port
 
-    public Client(String host, int port) {
-        this.appController = new AppController(host, port);
+    public Client(String host, int port, double width, double height) {
+        this.appController = new AppController(host, port, width, height);
     }
 
     //TODO
