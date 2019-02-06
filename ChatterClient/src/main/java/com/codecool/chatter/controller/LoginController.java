4893efe6966be83ad8ec2d3f26ead285ccfdc847
szymslow12 <szym.slow12@gmail.com>
2019-02-06 14:02:15 +0100

    remove unused import

--- ChatterClient/src/main/java/com/codecool/chatter/controller/LoginController.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/LoginController.java
@@ -12,7 +12,6 @@ import javafx.stage.StageStyle;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.Socket;
 
 public class LoginController {
