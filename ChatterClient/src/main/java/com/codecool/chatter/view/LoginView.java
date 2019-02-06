20306ee78cfc73a838357d1a1e7665ceb9b5e160
szymslow12 <szym.slow12@gmail.com>
2019-02-06 10:49:31 +0100

    change -> remove field width,height and sets super width and height

--- ChatterClient/src/main/java/com/codecool/chatter/view/LoginView.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/LoginView.java
@@ -11,15 +11,11 @@ public class LoginView extends Pane {
 
     private InputField inputField;
     private ButtonView buttonView;
-    private int width;
-    private int height;
-
-
 
     public LoginView(int width, int height, int margin, EventHandler<MouseEvent> onClick) {
         super();
-        this.width = width;
-        this.height = height;
+        setWidth(width);
+        setHeight(height);
         this.buttonView = new ButtonView().getButton("Join", onClick);
         createNicknameInputField(margin);
         renderLoginView();
@@ -28,20 +24,25 @@ public class LoginView extends Pane {
 
     private void createNicknameInputField(int margin) {
         Insets margins = new Insets(margin, margin ,margin,margin);
-        double width =  this.width * 0.75;
+        double width =  getWidth() * 0.75;
         double height = width * 0.25;
         this.inputField = new InputField("Nickname", false, width, height, margins);
     }
 
 
     private void renderLoginView() {
-        getChildren().add(inputField);
-        getChildren().add(buttonView);
+        double x = getWidth() / 2;
+        double y = getHeight() / 2;
+        inputField.setLayoutX(x - inputField.getWidth() / 2);
+        inputField.setLayoutY(y - inputField.getHeight() / 2);
+        buttonView.setLayoutX(x - buttonView.getWidth() / 2);
+//        buttonView.setLayoutY(y);
+        getChildren().addAll(inputField, buttonView);
         setBackground(
             new Background(
                 new BackgroundFill(Color.web("#66ffff"), CornerRadii.EMPTY, Insets.EMPTY)
             )
         );
-        setMinSize(width, height);
+        setMinSize(getWidth(), getHeight());
     }
 }
