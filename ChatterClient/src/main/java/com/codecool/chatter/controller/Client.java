55328e67117c51b6429ec09f07ccf90a5e2d9477
szymslow12 <szym.slow12@gmail.com>
2019-02-06 13:54:55 +0100

    remove fields host, port and run() does not create Socket

--- ChatterClient/src/main/java/com/codecool/chatter/controller/Client.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/Client.java
@@ -3,25 +3,18 @@ package com.codecool.chatter.controller;
 import javafx.scene.Scene;
 
 import java.io.IOException;
-import java.net.Socket;
 
 public class Client {
 
-    private String host;
-    private int port;
     private AppController appController;
 
     public Client(String host, int port) {
-        this.host = host;
-        this.port = port;
-        this.appController = new AppController();
+        this.appController = new AppController(host, port);
     }
 
     //TODO
     public void run() {
-        try (Socket socket = new Socket(host, port)) {
-            System.out.println("Client is running!");
-            appController.setConnection(socket);
+        try {
             appController.run();
         } catch (IOException e) {
             e.printStackTrace();
