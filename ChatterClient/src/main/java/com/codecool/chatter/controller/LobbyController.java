c5d519c6671824935615f4185c950e99db1bf12e
szymslow12 <szym.slow12@gmail.com>
2019-02-07 17:03:13 +0100

    change Long chosenRoomId to Room chosenRoom, add getChosenRoom,getRoomFromLobby

--- ChatterClient/src/main/java/com/codecool/chatter/controller/LobbyController.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/LobbyController.java
@@ -2,6 +2,7 @@ package com.codecool.chatter.controller;
 
 import com.codecool.chatter.ChatterClient;
 import com.codecool.chatter.model.Lobby;
+import com.codecool.chatter.model.Room;
 import com.codecool.chatter.view.AppView;
 import com.codecool.chatter.view.ButtonView;
 import com.codecool.chatter.view.LobbyView;
@@ -13,24 +14,46 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.Optional;
 
 public class LobbyController {
 
     private Socket connection;
     private Lobby lobby;
     private LobbyView lobbyView;
-    private Long chosenRoomId;
+    private Room chosenRoom;
 
     private EventHandler<MouseEvent> enterRoom = e -> {
         ButtonView buttonView = (ButtonView) e.getSource();
-        chosenRoomId = buttonView.getRoomId();
-        System.out.println(chosenRoomId);
+        long chosenRoomId = buttonView.getRoomId();
+        try {
+            DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeLong(chosenRoomId);
+            chosenRoom = getRoomFromLobby(chosenRoomId);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
     };
+
+
+    private Room getRoomFromLobby(long chosenId) {
+        Optional<Room> optionalRoom = lobby.getRooms().stream().filter(room -> room.getId() == chosenId).findFirst();
+        if (optionalRoom.isPresent()) {
+            return optionalRoom.get();
+        }
+        return null;
+    }
 
     public LobbyController(Socket connection) {
         this.connection = connection;
         lobby = null;
-        chosenRoomId = null;
+        chosenRoom = null;
+    }
+
+
+    public Room getChosenRoom() {
+        return chosenRoom;
     }
 
 
@@ -39,18 +62,12 @@ public class LobbyController {
         getLobbyFromServer();
         lobbyView.renderLobbyView(lobby, enterRoom);
         Platform.runLater(() -> appView.getChildren().add(lobbyView));
-        while (chosenRoomId == null) {
-
-        }
-        DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-        outputStream.writeLong(chosenRoomId);
-
     }
 
 
     private void getLobbyFromServer() throws IOException, ClassNotFoundException {
         ObjectInputStream inputStream = new ObjectInputStream(connection.getInputStream());
         lobby = (Lobby) inputStream.readObject();
-        System.out.println("Lobby has been loaded! " + lobby);
+        System.out.println("Lobby has been loaded!");
     }
 }
