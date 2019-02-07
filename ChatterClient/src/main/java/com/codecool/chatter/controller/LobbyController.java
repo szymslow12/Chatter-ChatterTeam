65d8caf68dfe769565306faa85e914fdc64e81a3
szymslow12 <szym.slow12@gmail.com>
2019-02-07 18:24:23 +0100

    remove getRoomFromLobby, change enterRoom

--- ChatterClient/src/main/java/com/codecool/chatter/controller/LobbyController.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/LobbyController.java
@@ -4,16 +4,15 @@ import com.codecool.chatter.ChatterClient;
 import com.codecool.chatter.model.Lobby;
 import com.codecool.chatter.model.Room;
 import com.codecool.chatter.view.AppView;
-import com.codecool.chatter.view.ButtonView;
 import com.codecool.chatter.view.LobbyView;
+import com.codecool.chatter.view.RoomButton;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Optional;
 
 public class LobbyController {
 
@@ -23,27 +22,18 @@ public class LobbyController {
     private Room chosenRoom;
 
     private EventHandler<MouseEvent> enterRoom = e -> {
-        ButtonView buttonView = (ButtonView) e.getSource();
-        long chosenRoomId = buttonView.getRoomId();
+        RoomButton roomButton = (RoomButton) e.getSource();
         try {
-            DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-            outputStream.writeLong(chosenRoomId);
-            outputStream.flush();
-            chosenRoom = getRoomFromLobby(chosenRoomId);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(connection.getOutputStream());
+            chosenRoom = roomButton.getRoom();
+            objectOutputStream.writeObject(chosenRoom);
+            objectOutputStream.flush();
         } catch (IOException e1) {
             e1.printStackTrace();
         }
 
     };
 
-
-    private Room getRoomFromLobby(long chosenId) {
-        Optional<Room> optionalRoom = lobby.getRooms().stream().filter(room -> room.getId() == chosenId).findFirst();
-        if (optionalRoom.isPresent()) {
-            return optionalRoom.get();
-        }
-        return null;
-    }
 
     public LobbyController(Socket connection) {
         this.connection = connection;
