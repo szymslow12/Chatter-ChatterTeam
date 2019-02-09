e84bd36d37095c7c13cd884bcc90ff30fcdda399
Riczard <riczardkiller@gmail.com>
2019-02-09 15:55:34 +0100

    Add Object wrapper

---
+++ ChatterServer/src/main/java/com/codecool/chatter/ObjectWrapper.java
@@ -0,0 +1,21 @@
+package com.codecool.chatter;
+
+public class ObjectWrapper {
+
+    private String action;
+
+    private Object object;
+
+    public ObjectWrapper(String action, Object object) {
+        this.action = action;
+        this.object = object;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+}
