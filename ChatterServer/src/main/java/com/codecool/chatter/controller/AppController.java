fc4f598671bda26ba3f59c44a5b1571c5418152b
Riczard <riczardkiller@gmail.com>
2019-02-09 17:08:27 +0100

    add method handle for future growing

--- ChatterServer/src/main/java/com/codecool/chatter/controller/AppController.java
+++ ChatterServer/src/main/java/com/codecool/chatter/controller/AppController.java
@@ -1,5 +1,6 @@
 package com.codecool.chatter.controller;
 
+import com.codecool.chatter.ObjectWrapper;
 import com.codecool.chatter.model.Lobby;
 
 import java.io.IOException;
@@ -22,5 +23,19 @@ public class AppController {
         return lobby.getUsers().stream().anyMatch(user -> userName.equalsIgnoreCase(user.getNickname()));
     }
 
+    public Object wrapObject(String action, Object object) {
+        return new ObjectWrapper(action, object);
+    }
+
+    public void handleData(Object object) {
+        ObjectWrapper data = (ObjectWrapper) object;
+        String action = data.getAction();
+        switch (action) {
+            case "login":
+                break;
+            case "":
+        }
+
+    }
 
 }
