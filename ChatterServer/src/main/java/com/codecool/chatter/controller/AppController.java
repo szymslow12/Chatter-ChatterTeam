508dcef0716b4e775df1984cb654a7eba02a5535
Riczard <riczardkiller@gmail.com>
2019-02-11 16:56:08 +0100

    delete login handle and implement chosenRoomById method

--- ChatterServer/src/main/java/com/codecool/chatter/controller/AppController.java
+++ ChatterServer/src/main/java/com/codecool/chatter/controller/AppController.java
@@ -1,11 +1,18 @@
 package com.codecool.chatter.controller;
 
-import com.codecool.chatter.model.ObjectWrapper;
 import com.codecool.chatter.model.Lobby;
+import com.codecool.chatter.model.ObjectWrapper;
+import com.codecool.chatter.model.Room;
+import com.codecool.chatter.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppController {
 
     private Lobby lobby;
+
+    private List<User> allUsers = new ArrayList<>();
 
     public AppController() {
         this.lobby = new Lobby();
@@ -19,22 +26,36 @@ public class AppController {
         return new ObjectWrapper(action, object);
     }
 
-    public Object handleData(Object object) {
+    public Object handleData(Object object, User user) {
         ObjectWrapper data = (ObjectWrapper) object;
         Object answer = null;
         String action = data.getAction();
         switch (action) {
-            case "login":
-                answer = checkNickNameExist((String) data.getObject());
-                break;
-            case "lobby":
-                answer = lobby;
+            case "chosenRoomId":
+                answer = chooseRoomHandle((long) data.getObject(), user);
                 break;
         }
         return answer;
     }
 
     public boolean checkNickNameExist(String userName) {
-        return lobby.getUsers().stream().anyMatch(user -> userName.equalsIgnoreCase(user.getNickname()));
+        return allUsers.stream().anyMatch(user -> userName.equalsIgnoreCase(user.getNickname()));
+    }
+
+    private Object chooseRoomHandle(long id, User user) {
+        if(checkRoomByIdExist(id)) {
+            Room room = getRoomById(id);
+            room.addUser(user);
+            return room;
+        }
+        return IllegalArgumentException.class;
+    }
+
+    private boolean checkRoomByIdExist(long id) {
+        return lobby.getRooms().stream().anyMatch(room -> id == room.getId());
+    }
+
+    private Room getRoomById(long id) {
+        return lobby.getRooms().stream().filter(room -> room.getId() == id).findFirst().get();
     }
 }
