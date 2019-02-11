7d0e0efa3facb8d224f67174742c776d48dea7bf
szymslow12 <szym.slow12@gmail.com>
2019-02-11 23:05:18 +0100

    add setFill, setFont, change renderLobbyIndoView -> has Text as children

--- ChatterClient/src/main/java/com/codecool/chatter/view/LobbyInfoView.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/LobbyInfoView.java
@@ -4,9 +4,13 @@ import com.codecool.chatter.model.Lobby;
 import com.codecool.chatter.model.Room;
 import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class LobbyInfoView extends GridPane {
 
@@ -24,6 +28,27 @@ public class LobbyInfoView extends GridPane {
         List<Room> rooms = lobby.getRooms();
         long roomsNumber = rooms.size();
         long usersNumber = IntStream.range(0, rooms.size()).mapToLong(i -> rooms.get(i).getUsers().size()).sum();
-        System.out.println(roomsNumber + " - " + usersNumber);
+        Font font = new Font(25);
+        Text roomNumberText = new Text("Rooms number: ");
+        Text roomNumber = new Text(Long.toString(roomsNumber));
+        Text userNumberText = new Text("Users number: ");
+        Text userNumber = new Text(Long.toString(usersNumber));
+        setFill(roomNumberText, roomNumber, userNumberText, userNumber);
+        setFont(font, roomNumberText, roomNumber, userNumberText, userNumber);
+        setConstraints(roomNumberText, 0, 0);
+        setConstraints(roomNumber, 1, 0);
+        setConstraints(userNumberText, 0, 1);
+        setConstraints(userNumber, 1, 1);
+        getChildren().addAll(roomNumberText, roomNumber, userNumberText, userNumber);
+    }
+
+
+    private void setFill(Text... texts) {
+        Stream.of(texts).forEach(text -> text.setFill(Color.web("#000000")));
+    }
+
+
+    private void setFont(Font font, Text... texts) {
+        Stream.of(texts).forEach(text -> text.setFont(font));
     }
 }
