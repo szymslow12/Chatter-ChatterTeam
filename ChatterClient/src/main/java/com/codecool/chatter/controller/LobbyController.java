82ba1b4d20f4f16a371deb6814220889b26ff347
szymslow12 <szym.slow12@gmail.com>
2019-02-12 23:27:16 +0100

    refactor -> move createRoom as handle to CreateRoom, add getLobbyView

--- ChatterClient/src/main/java/com/codecool/chatter/controller/LobbyController.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/LobbyController.java
@@ -1,14 +1,10 @@
 package com.codecool.chatter.controller;
 
 import com.codecool.chatter.ChatterClient;
+import com.codecool.chatter.controller.eventHandler.CreateRoom;
 import com.codecool.chatter.controller.eventHandler.EnterRoom;
 import com.codecool.chatter.model.*;
 import com.codecool.chatter.view.*;
-import com.codecool.chatter.view.box.CreateRoomForm;
-import javafx.event.EventHandler;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.control.TextInputControl;
-import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
@@ -19,31 +15,6 @@ public class LobbyController {
     private LobbyView lobbyView;
     private Room chosenRoom;
 
-
-    private EventHandler<MouseEvent> createRoom = e -> {
-        CreateRoomForm createRoomForm = lobbyView.getCreateRoomForm();
-       TextInputControl textInputControl = createRoomForm.getInputField().getTextInputControl();
-       String roomName = textInputControl.getText();
-       ObjectWrapper objectWrapper = new ObjectWrapper("createRoom", roomName);
-        try {
-            connection.write(objectWrapper);
-            objectWrapper = connection.read();
-            if (objectWrapper.getAction().equals("isAvailable")) {
-                Canvas alert = createRoomForm.getAlertMessage();
-                boolean isDisplayedAlert = createRoomForm.getChildren().contains(alert);
-                if (!isDisplayedAlert) {
-                    createRoomForm.getChildren().add(alert);
-                }
-            } else {
-                chosenRoom = (Room) objectWrapper.getObject();
-            }
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        } catch (ClassNotFoundException e1) {
-            e1.printStackTrace();
-        }
-
-    };
 
     public LobbyController(Connection connection) {
         this.connection = connection;
@@ -68,10 +39,15 @@ public class LobbyController {
     }
 
 
+    public LobbyView getLobbyView() {
+        return lobbyView;
+    }
+
+
     public void run(AppView appView, User client) {
         try {
             getLobbyFromServer();
-            lobbyView.renderLobbyView(lobby, client, new EnterRoom(this), createRoom);
+            lobbyView.renderLobbyView(lobby, client, new EnterRoom(this), new CreateRoom(this));
             appView.getChildren().add(lobbyView);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
--- ChatterClient/src/main/java/com/codecool/chatter/controller/eventHandler/CreateRoom.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/eventHandler/CreateRoom.java
@@ -1,19 +1,55 @@
 package com.codecool.chatter.controller.eventHandler;
 
+import com.codecool.chatter.controller.LobbyController;
 import com.codecool.chatter.model.Connection;
+import com.codecool.chatter.model.ObjectWrapper;
+import com.codecool.chatter.model.Room;
+import com.codecool.chatter.view.LobbyView;
+import com.codecool.chatter.view.box.CreateRoomForm;
 import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.TextInputControl;
 import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
 
 public class CreateRoom implements EventHandler<MouseEvent> {
 
-    private Connection connection;
+    private LobbyController lobbyController;
 
-    public CreateRoom(Connection connection) {
-        this.connection = connection;
+    public CreateRoom(LobbyController lobbyController) {
+        this.lobbyController = lobbyController;
     }
 
     @Override
     public void handle(MouseEvent mouseEvent) {
+        Connection connection = lobbyController.getConnection();
+        LobbyView lobbyView = lobbyController.getLobbyView();
+        CreateRoomForm createRoomForm = lobbyView.getCreateRoomForm();
+        TextInputControl textInputControl = createRoomForm.getInputField().getTextInputControl();
+        String roomName = textInputControl.getText();
+        ObjectWrapper objectWrapper = new ObjectWrapper("createRoom", roomName);
+        try {
+            connection.write(objectWrapper);
+            objectWrapper = connection.read();
+            checkIfIsRoomNameAvailable(objectWrapper, createRoomForm);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+    };
 
+
+    private void checkIfIsRoomNameAvailable(ObjectWrapper objectWrapper, CreateRoomForm createRoomForm) {
+        if (objectWrapper.getAction().equals("isAvailable")) {
+            Canvas alert = createRoomForm.getAlertMessage();
+            boolean isDisplayedAlert = createRoomForm.getChildren().contains(alert);
+            if (!isDisplayedAlert) {
+                createRoomForm.getChildren().add(alert);
+            }
+        } else {
+            lobbyController.setChosenRoom((Room) objectWrapper.getObject());
+        }
     }
 }
