f5461c45dda904e025b1897a4a0ab803a66b6257
szymslow12 <szym.slow12@gmail.com>
2019-02-12 17:28:29 +0100

    refactor -> remove all with badRoomName -> add instead AlertMessage

--- ChatterClient/src/main/java/com/codecool/chatter/view/CreateRoomView.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/CreateRoomView.java
@@ -16,12 +16,13 @@ public class CreateRoomView extends Pane {
 
     private InputField inputField;
     private ButtonView buttonView;
-    private Canvas badRoomName;
+    private AlertMessage alertMessage;
 
     public CreateRoomView(double width, double height) {
         super();
         inputField = new InputField("Room name:", false, width * 0.9, height * 0.25, new Insets(10));
         buttonView = new ButtonView((width - 20) * 0.25, height * 0.1);
+        alertMessage = new AlertMessage(width, height / 2, "Bad room name!");
         setWidth(width);
         setHeight(height);
         setPrefSize(width, height);
@@ -45,7 +46,9 @@ public class CreateRoomView extends Pane {
 //        );
         setBorder(getCreateRoomBorder());
         setPositions();
-        renderBadRoomNameAlert();
+        double x = (getWidth() - inputField.getWidth()) / 2;
+        double y = 10;
+        alertMessage.renderAlertMessage(x, y);
         getChildren().addAll(inputField, buttonView);
     }
 
@@ -79,45 +82,7 @@ public class CreateRoomView extends Pane {
     }
 
 
-    public Canvas getBadRoomName() {
-        return badRoomName;
-    }
-
-
-    private void renderBadRoomNameAlert() {
-        double x = (getWidth() - inputField.getWidth()) / 2;
-        double y = 10;
-        badRoomName = new Canvas(inputField.getWidth(), inputField.getHeight() / 2);
-        drawBadLoginTry(badRoomName);
-        badRoomName.setLayoutX(x);
-        badRoomName.setLayoutY(10);
-    }
-
-
-    private void drawBadLoginTry(Canvas canvas) {
-        GraphicsContext context = canvas.getGraphicsContext2D();
-        double width = inputField.getWidth();
-        double height = inputField.getHeight() / 2;
-        double x = 0; double y = 0;
-        drawBox(x, y, width, height, context);
-        drawMessage(x, y, width, height, context);
-    }
-
-
-    private void drawBox(double x, double y, double width, double height, GraphicsContext context) {
-        context.strokeRect(x, y, width, height);
-        context.setFill(Color.web("rgba(255, 0, 0, 0.8)"));
-        context.fillRect(x + 1, y + 1, width - 1, height - 1);
-    }
-
-
-    private void drawMessage(double x, double y, double width, double height, GraphicsContext context) {
-        String badLoginMessage = "Room name is used!";
-        Bounds bounds = new Text(badLoginMessage).getLayoutBounds();
-        double textX = x + width / 2 - bounds.getWidth();
-        double textY = y + ((height + 1) / 2) + bounds.getHeight() / 2;
-        context.setFill(Color.web("#000000"));
-        context.setFont(new Font(25));
-        context.fillText(badLoginMessage, textX, textY);
+    public AlertMessage getBadRoomName() {
+        return alertMessage;
     }
 }
