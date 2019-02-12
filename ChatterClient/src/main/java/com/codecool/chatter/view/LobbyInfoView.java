cd387af7542ab374fae48399f45c426163507f29
szymslow12 <szym.slow12@gmail.com>
2019-02-12 15:11:27 +0100

    add setConstraints and add new Text to GridPane -> username

--- ChatterClient/src/main/java/com/codecool/chatter/view/LobbyInfoView.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/LobbyInfoView.java
@@ -31,18 +31,17 @@ public class LobbyInfoView extends GridPane {
         long roomsNumber = rooms.size();
         long usersNumber = IntStream.range(0, rooms.size()).mapToLong(i -> rooms.get(i).getUsers().size()).sum();
         Font font = new Font(25);
+        Text userNameText = new Text("Username:");
+        Text userName = new Text(client.getNickname());
         Text roomNumberText = new Text("Rooms:");
         Text roomNumber = new Text(Long.toString(roomsNumber));
         Text userNumberText = new Text("Users:");
         Text userNumber = new Text(Long.toString(usersNumber));
-        setFont(font, roomNumberText, roomNumber, userNumberText, userNumber);
-        setFill(Color.web("#000000"), roomNumberText, userNumberText);
-        setFill(Color.web("#00cc00"), roomNumber, userNumber);
-        setConstraints(roomNumberText, 0, 0);
-        setConstraints(roomNumber, 1, 0);
-        setConstraints(userNumberText, 0, 1);
-        setConstraints(userNumber, 1, 1);
-        getChildren().addAll(roomNumberText, roomNumber, userNumberText, userNumber);
+        setFont(font, userNameText, userName, roomNumberText, roomNumber, userNumberText, userNumber);
+        setFill(Color.web("#000000"), roomNumberText, userNumberText, userNameText);
+        setFill(Color.web("#00cc00"), roomNumber, userNumber, userName);
+        setConstraints(userNameText, userName, roomNumberText, roomNumber, userNumberText, userNumber);
+        getChildren().addAll(roomNumberText, roomNumber, userNumberText, userNumber, userNameText, userName);
     }
 
 
@@ -53,5 +52,17 @@ public class LobbyInfoView extends GridPane {
 
     private void setFont(Font font, Text... texts) {
         Stream.of(texts).forEach(text -> text.setFont(font));
+    }
+
+
+    private void setConstraints(Text userNameText, Text userName, Text roomNumberText,
+                                Text roomNumber, Text userNumberText, Text userNumber) {
+
+        setConstraints(userNameText, 0, 0);
+        setConstraints(userName, 1, 0);
+        setConstraints(roomNumberText, 0, 2);
+        setConstraints(roomNumber, 1, 2);
+        setConstraints(userNumberText, 0, 4);
+        setConstraints(userNumber, 1, 4);
     }
 }
