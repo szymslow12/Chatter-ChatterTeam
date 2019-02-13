c0dd4a9414c0fea990603dd112b1612abb39a935
szymslow12 <szym.slow12@gmail.com>
2019-02-13 22:48:12 +0100

    add commented fields UserListBox, ChatBox, add adding to scene

--- ChatterClient/src/main/java/com/codecool/chatter/view/RoomView.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/RoomView.java
@@ -1,9 +1,17 @@
 package com.codecool.chatter.view;
 
 import com.codecool.chatter.model.Room;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class RoomView extends Pane {
+
+    //private UserListBox userListBox;
+    //private ChatBox chatBox;
 
 
     public RoomView(double width, double height) {
@@ -15,6 +23,16 @@ public class RoomView extends Pane {
 
 
     public void renderRoomView(Room room) {
-
+        setBackground(
+            new Background(
+                new BackgroundFill(
+                        Color.web("rgba(255, 255, 255, 0.4)"),
+                        CornerRadii.EMPTY,
+                        Insets.EMPTY)
+            )
+        );
+        //userListBox.renderUserListBox(room.getName(), room.getUsers());
+        //chatBox.renderChatBox(room.getChat());
+        //getChildren().addAll(userListBox, chatBox);
     }
 }
