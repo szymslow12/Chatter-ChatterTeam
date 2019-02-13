1d6b087596fb8139fb59d5b3006d6c92ac1f117f
Riczard <riczardkiller@gmail.com>
2019-02-13 13:51:03 +0100

    change generate ID in room

--- ChatterServer/src/main/java/com/codecool/chatter/model/Room.java
+++ ChatterServer/src/main/java/com/codecool/chatter/model/Room.java
@@ -3,41 +3,32 @@ package com.codecool.chatter.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Room implements Serializable {
 
-    private static long id = 1;
+    private UUID id = UUID.randomUUID();
     private String name;
     private int maxCapacity;
     private List<User> users;
-    private int port;
     private Chat chat;
 
 
-    public Room(String name, int port) {
-        id++;
+    public Room(String name) {
         this.name = name;
-        this.port = port;
         this.maxCapacity = 20;
         this.users = new ArrayList<>();
     }
 
 
     public void addUser(User user) {
-        user.setCurrentRoomId(id);
         users.add(user);
     }
 
 
-    public long getId() {
+    public UUID getId() {
         return id;
     }
-
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
 
     public String getName() {
         return name;
@@ -67,17 +58,6 @@ public class Room implements Serializable {
     public void setMaxCapacity(int maxCapacity) {
         this.maxCapacity = maxCapacity;
     }
-
-
-    public int getPort() {
-        return port;
-    }
-
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
 
     public List<User> getUsers() {
         return users;
