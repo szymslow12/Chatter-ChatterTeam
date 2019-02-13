66c977129ef60a94b0924e60741e640b4f6589b4
szymslow12 <szym.slow12@gmail.com>
2019-02-13 22:43:38 +0100

    add displaying RoomView

--- ChatterClient/src/main/java/com/codecool/chatter/controller/RoomController.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/RoomController.java
@@ -1,25 +1,31 @@
 package com.codecool.chatter.controller;
 
+import com.codecool.chatter.ChatterClient;
 import com.codecool.chatter.model.Connection;
 import com.codecool.chatter.model.Room;
 import com.codecool.chatter.model.User;
 import com.codecool.chatter.view.AppView;
+import com.codecool.chatter.view.RoomView;
 
 import java.io.IOException;
 
 public class RoomController {
 
     private Connection connection;
-//    private RoomView roomView;
+    private RoomView roomView;
 
     public RoomController(Connection connection) {
         this.connection = connection;
+        this.roomView = new RoomView(ChatterClient.WIDTH, ChatterClient.HEIGHT);
     }
 
 
     public void run(AppView appView, User client) {
         try {
             Room room = (Room) connection.read().getObject();
+            room.getChat().setClient(client);
+            roomView.renderRoomView(room);
+            appView.getChildren().add(appView);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
