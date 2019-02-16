349dd359297949d70a207ee26561d681d8651cce
szymslow12 <szym.slow12@gmail.com>
2019-02-16 20:34:03 +0100

    add ChatBox -> extends ScrollContainer, add renderChatBox

---
+++ ChatterClient/src/main/java/com/codecool/chatter/view/containers/ChatBox.java
@@ -0,0 +1,31 @@
+package com.codecool.chatter.view.containers;
+
+import com.codecool.chatter.model.Chat;
+import com.codecool.chatter.model.Message;
+import com.codecool.chatter.view.alert.MessageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatBox extends ScrollContainer {
+
+    private List<MessageView> messages;
+
+    public ChatBox(double width, double height) {
+        super(width, height);
+        messages = new ArrayList<>();
+    }
+
+
+    public void renderChatBox(Chat chat) {
+        List<Message> messageList = chat.getMessages();
+        messageList.stream().forEach(message -> addMessageView(message));
+    }
+
+
+    private void addMessageView(Message message) {
+        MessageView messageView = new MessageView(getWidth() * 0.8, 50, message);
+        messages.add(messageView);
+        getChildren().add(messageView);
+    }
+}
