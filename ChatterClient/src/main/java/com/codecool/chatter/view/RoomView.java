66ba2c01d41b071a0ae0e67fc365007ab224dbdb
szymslow12 <szym.slow12@gmail.com>
2019-02-16 20:39:59 +0100

    impl renderRoomView

--- ChatterClient/src/main/java/com/codecool/chatter/view/RoomView.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/RoomView.java
@@ -3,6 +3,7 @@ package com.codecool.chatter.view;
 import com.codecool.chatter.ChatterClient;
 import com.codecool.chatter.model.Room;
 import com.codecool.chatter.view.containers.UserListBox;
+import com.codecool.chatter.view.form.ChatForm;
 import com.codecool.chatter.view.interactive.RoomButton;
 import javafx.geometry.Insets;
 import javafx.scene.layout.Background;
@@ -14,12 +15,13 @@ import javafx.scene.paint.Color;
 public class RoomView extends Pane {
 
     private UserListBox userListBox;
-    //private ChatView chatBox;
+    private ChatForm chatForm;
 
 
     public RoomView(double width, double height) {
         super();
         userListBox = new UserListBox(300d, ChatterClient.HEIGHT - 100);
+        chatForm = new ChatForm(ChatterClient.WIDTH - 300d, ChatterClient.HEIGHT);
         setSizes(width, height);
     }
 
@@ -43,8 +45,8 @@ public class RoomView extends Pane {
         RoomButton titleRoomButton = new RoomButton(userListBox.getWidth() + 30, 100, room);
         userListBox.renderUserListBox(room);
         userListBox.setTranslateY(100);
-        //chatBox.renderChatBox(room.getChat());
-//        getChildren().addAll(userListBox, chatBox);
-        getChildren().addAll(titleRoomButton, userListBox);
+        chatForm.renderChatForm(room.getChat());
+        chatForm.setTranslateX(userListBox.getWidth());
+        getChildren().addAll(titleRoomButton, userListBox, chatForm);
     }
 }
