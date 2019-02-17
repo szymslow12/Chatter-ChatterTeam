aa417e7e7bf03102f9f557176265737455708926
szymslow12 <szym.slow12@gmail.com>
2019-02-17 15:50:12 +0100

    change Text size in renderMessageView

--- ChatterClient/src/main/java/com/codecool/chatter/view/alert/MessageView.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/alert/MessageView.java
@@ -3,6 +3,8 @@ package com.codecool.chatter.view.alert;
 import com.codecool.chatter.model.Message;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class MessageView extends Canvas {
 
@@ -13,8 +15,13 @@ public class MessageView extends Canvas {
 
 
     private void renderMessageView(Message message) {
+        String messageString = getMessageString(message);
         GraphicsContext context = getGraphicsContext2D();
-        context.fillText(getMessageString(message), 0, 0);
+        Font font = new Font(15);
+        Text text = new Text(messageString);
+        text.setFont(font);
+        context.setFont(font);
+        context.fillText(messageString, 0, getHeight() - text.getLayoutBounds().getHeight() / 2);
     }
 
 
