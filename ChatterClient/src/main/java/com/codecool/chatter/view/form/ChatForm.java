fb28cae718476a2a94e830ce98a6af441ab4accb
szymslow12 <szym.slow12@gmail.com>
2019-02-17 16:42:48 +0100

    add setInputField

--- ChatterClient/src/main/java/com/codecool/chatter/view/form/ChatForm.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/form/ChatForm.java
@@ -3,8 +3,12 @@ package com.codecool.chatter.view.form;
 import com.codecool.chatter.model.Chat;
 import com.codecool.chatter.view.containers.ChatBox;
 import com.codecool.chatter.view.interactive.InputField;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 
 public class ChatForm extends Pane {
 
@@ -20,7 +24,20 @@ public class ChatForm extends Pane {
 
     public void renderChatForm(Chat chat) {
         chatBox.renderChatBox(chat);
-        inputField.setTranslateY(chatBox.getHeight());
+        setInputField();
         getChildren().addAll(chatBox, inputField);
+    }
+
+
+    private void setInputField() {
+        EventHandler<KeyEvent> onEnter = e -> {
+            if (e.getCode() == KeyCode.ENTER) {
+                String message = inputField.getTextInputControl().getText();
+                System.out.println(message);
+            }
+        };
+        inputField.setTranslateY(chatBox.getHeight());
+        inputField.setOnEnterPressed(onEnter);
+        inputField.setFont(new Font(20));
     }
 }
