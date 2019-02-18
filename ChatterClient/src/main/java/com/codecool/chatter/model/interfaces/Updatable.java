4929fd41028c8fa33a277fda6846bfc68e440fe5
szymslow12 <szym.slow12@gmail.com>
2019-02-18 18:55:54 +0100

    change type from generic to InputEvent in updateView

--- ChatterClient/src/main/java/com/codecool/chatter/model/interfaces/Updatable.java
+++ ChatterClient/src/main/java/com/codecool/chatter/model/interfaces/Updatable.java
@@ -3,8 +3,9 @@ package com.codecool.chatter.model.interfaces;
 import com.codecool.chatter.model.ObjectWrapper;
 import com.codecool.chatter.model.User;
 import javafx.event.EventHandler;
+import javafx.scene.input.InputEvent;
 
 public interface Updatable {
 
-    void updateView(ObjectWrapper objectWrapper, User client, EventHandler<?> eventHandler);
+    void updateView(ObjectWrapper objectWrapper, User client, EventHandler<InputEvent> eventHandler);
 }
