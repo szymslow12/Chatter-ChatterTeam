6c3147328ad089c4016262ab8cfa1156c69eff3e
szymslow12 <szym.slow12@gmail.com>
2019-02-18 09:45:08 +0100

    add field User client and assign it in constructor, add updateChat

--- ChatterClient/src/main/java/com/codecool/chatter/controller/RoomController.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/RoomController.java
@@ -8,22 +8,34 @@ import com.codecool.chatter.model.User;
 import com.codecool.chatter.view.AppView;
 import com.codecool.chatter.view.RoomView;
 
+import java.io.IOException;
+
 public class RoomController {
 
     private Connection connection;
     private RoomView roomView;
+    private User client;
 
-    public RoomController(Connection connection) {
+    public RoomController(Connection connection, User client) {
         this.connection = connection;
         this.roomView = new RoomView(ChatterClient.WIDTH, ChatterClient.HEIGHT);
+        this.client = client;
     }
 
 
-    public void run(AppView appView, User client, Room chosenRoom) {
+    public void run(AppView appView, Room chosenRoom) {
         client.setCurrentRoomId(chosenRoom.getId());
         chosenRoom.getChat().setClient(client);
         roomView.renderRoomView(chosenRoom, new SendMessage(connection, this, client));
         appView.getChildren().add(roomView);
+    }
+
+
+    public void updateChat() throws IOException, ClassNotFoundException {
+        Room roomToUpdate = (Room) connection.read().getObject();
+        roomToUpdate.getChat().setClient(client);
+        roomView.getChildren().clear();
+        roomView.renderRoomView(roomToUpdate, new SendMessage(connection, this, client));
     }
 
 
