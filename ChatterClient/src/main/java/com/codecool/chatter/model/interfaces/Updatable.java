b60a3eeb9d5a384e3ca5c0e875be40faa6e67550
szymslow12 <szym.slow12@gmail.com>
2019-02-18 18:34:53 +0100

    add EventHandler param to updateView

--- ChatterClient/src/main/java/com/codecool/chatter/model/interfaces/Updatable.java
+++ ChatterClient/src/main/java/com/codecool/chatter/model/interfaces/Updatable.java
@@ -1,8 +1,9 @@
 package com.codecool.chatter.model.interfaces;
 
 import com.codecool.chatter.model.ObjectWrapper;
+import javafx.event.EventHandler;
 
 public interface Updatable {
 
-    void updateView(ObjectWrapper objectWrapper);
+    void updateView(ObjectWrapper objectWrapper, EventHandler<?> eventHandler);
 }
