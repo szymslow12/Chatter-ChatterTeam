41331fc7d7ffe41432f195d3921a535a5f5efa4e
szymslow12 <szym.slow12@gmail.com>
2019-02-18 09:29:44 +0100

    change -> setItemsSpacing to 0

--- ChatterClient/src/main/java/com/codecool/chatter/view/containers/ChatBox.java
+++ ChatterClient/src/main/java/com/codecool/chatter/view/containers/ChatBox.java
@@ -20,6 +20,7 @@ public class ChatBox extends ScrollContainer {
     public void renderChatBox(Chat chat) {
         List<Message> messageList = chat.getMessages();
         messageList.stream().forEach(message -> addMessageView(message));
+        setItemsSpacing(0);
         getScrollPane().setVvalue(1.0);
         getChildren().addAll(getScrollPane(), getScrollBar());
     }
