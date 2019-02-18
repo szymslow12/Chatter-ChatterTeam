b72bc868e8eddc9d0c1b562c14b419e236a8d03c
szymslow12 <szym.slow12@gmail.com>
2019-02-18 18:43:35 +0100

    add param User client to updateView

--- ChatterClient/src/main/java/com/codecool/chatter/model/interfaces/Updatable.java
+++ ChatterClient/src/main/java/com/codecool/chatter/model/interfaces/Updatable.java
@@ -1,9 +1,10 @@
 package com.codecool.chatter.model.interfaces;
 
 import com.codecool.chatter.model.ObjectWrapper;
+import com.codecool.chatter.model.User;
 import javafx.event.EventHandler;
 
 public interface Updatable {
 
-    void updateView(ObjectWrapper objectWrapper, EventHandler<?> eventHandler);
+    void updateView(ObjectWrapper objectWrapper, User client, EventHandler<?> eventHandler);
 }
