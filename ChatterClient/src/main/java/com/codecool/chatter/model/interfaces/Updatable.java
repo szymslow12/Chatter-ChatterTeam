c0132d032949f1c501953b9eeb9f3cb33da6360c
szymslow12 <szym.slow12@gmail.com>
2019-02-18 18:30:08 +0100

    fix typo, add param ObjectWrapper to updateView

---
+++ ChatterClient/src/main/java/com/codecool/chatter/model/interfaces/Updatable.java
@@ -0,0 +1,8 @@
+package com.codecool.chatter.model.interfaces;
+
+import com.codecool.chatter.model.ObjectWrapper;
+
+public interface Updatable {
+
+    void updateView(ObjectWrapper objectWrapper);
+}
