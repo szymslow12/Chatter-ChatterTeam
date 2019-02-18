555c78bf269fb9e4695e78456a87d6953bccf9b3
szymslow12 <szym.slow12@gmail.com>
2019-02-18 10:24:32 +0100

    change -> remove field User, add field Room, add getRoom

--- ChatterClient/src/main/java/com/codecool/chatter/controller/RoomController.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/RoomController.java
@@ -8,38 +8,37 @@ import com.codecool.chatter.model.User;
 import com.codecool.chatter.view.AppView;
 import com.codecool.chatter.view.RoomView;
 
-import java.io.IOException;
-
 public class RoomController {
 
     private Connection connection;
     private RoomView roomView;
-    private User client;
+    private Room room;
 
-    public RoomController(Connection connection, User client) {
+    public RoomController(Connection connection, Room room) {
         this.connection = connection;
         this.roomView = new RoomView(ChatterClient.WIDTH, ChatterClient.HEIGHT);
-        this.client = client;
+        this.room = room;
     }
 
 
-    public void run(AppView appView, Room chosenRoom) {
-        client.setCurrentRoomId(chosenRoom.getId());
-        chosenRoom.getChat().setClient(client);
-        roomView.renderRoomView(chosenRoom, new SendMessage(connection, this, client));
+    public void run(AppView appView, User client) {
+        client.setCurrentRoomId(room.getId());
+        room.getChat().setClient(client);
+        roomView.renderRoomView(room, new SendMessage(connection, this, client));
         appView.getChildren().add(roomView);
     }
 
 
-    public void updateChat() throws IOException, ClassNotFoundException {
-        Room roomToUpdate = (Room) connection.read().getObject();
-        roomToUpdate.getChat().setClient(client);
-        roomView.getChildren().clear();
-        roomView.renderRoomView(roomToUpdate, new SendMessage(connection, this, client));
+    public void updateChat() {
+        roomView.clearChildren();
     }
 
 
     public RoomView getRoomView() {
         return roomView;
+    }
+
+    public Room getRoom() {
+        return room;
     }
 }
