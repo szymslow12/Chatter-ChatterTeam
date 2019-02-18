dd3e3ed64db9e55abcb1eea1c6ea0c34faf87808
Riczard <riczardkiller@gmail.com>
2019-02-18 11:26:38 +0100

    add new field for ClientInfo

--- ChatterServer/src/main/java/com/codecool/chatter/model/ClientInfo.java
+++ ChatterServer/src/main/java/com/codecool/chatter/model/ClientInfo.java
@@ -1,11 +1,14 @@
 package com.codecool.chatter.model;
 
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class ClientInfo {
 
     private ObjectOutputStream outputStream;
+    private ObjectInputStream inputStream;
     private User user;
+    private int latestMsgIndex;
 
     public ClientInfo(ObjectOutputStream outputStream, User user) {
         this.outputStream = outputStream;
@@ -18,5 +21,17 @@ public class ClientInfo {
 
     public User getUser() {
         return user;
+    }
+
+    public ObjectInputStream getInputStream() {
+        return inputStream;
+    }
+
+    public int getLatestMsgIndex() {
+        return latestMsgIndex;
+    }
+
+    public void setLatestMsgIndex(int latestMsgIndex) {
+        this.latestMsgIndex = latestMsgIndex;
     }
 }
