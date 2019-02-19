da4af7906f3a310a80c3c2e24e8e3b889c3b4902
szymslow12 <szym.slow12@gmail.com>
2019-02-19 18:30:22 +0100

    change param User to Object

--- ChatterClient/src/main/java/com/codecool/chatter/model/interfaces/Updatable.java
+++ ChatterClient/src/main/java/com/codecool/chatter/model/interfaces/Updatable.java
@@ -1,11 +1,10 @@
 package com.codecool.chatter.model.interfaces;
 
 import com.codecool.chatter.model.ObjectWrapper;
-import com.codecool.chatter.model.User;
 import javafx.event.EventHandler;
 import javafx.scene.input.InputEvent;
 
 public interface Updatable {
 
-    void updateView(ObjectWrapper objectWrapper, User client, EventHandler<InputEvent> eventHandler);
+    void updateView(ObjectWrapper objectWrapper, Object object, EventHandler<InputEvent> eventHandler);
 }
