e6299d0a0a9c04e2a37d32b387ed4e74e722e891
szymslow12 <szym.slow12@gmail.com>
2019-04-12 15:32:11 +0200

    change for test purposes sended object to String

--- ChatterServer/src/main/java/com/codecool/chatter/model/Connection.java
+++ ChatterServer/src/main/java/com/codecool/chatter/model/Connection.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.*;
+import java.util.stream.Stream;
 
 public class Connection {
 
@@ -50,7 +51,9 @@ public class Connection {
         ObjectOutputStream oos = new ObjectOutputStream(bos);
         oos.writeObject(objectWrapper);
         buf = bos.toByteArray();
-        DatagramPacket response = new DatagramPacket(buf, buf.length, inetAddress, 8081);
+        String action = objectWrapper.getAction();
+        byte[] actionsBytes = action.getBytes();
+        DatagramPacket response = new DatagramPacket(actionsBytes, actionsBytes.length, inetAddress, 8081);
         datagramSocket.send(response);
     }
 
