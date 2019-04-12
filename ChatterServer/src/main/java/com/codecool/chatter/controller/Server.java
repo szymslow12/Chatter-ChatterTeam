8b879594842b0cfc034dd46939e9422a2547f9f8
Riczard <riczardkiller@gmail.com>
2019-04-12 13:50:07 +0200

    add udp connections

--- ChatterServer/src/main/java/com/codecool/chatter/controller/Server.java
+++ ChatterServer/src/main/java/com/codecool/chatter/controller/Server.java
@@ -3,6 +3,8 @@ package com.codecool.chatter.controller;
 import com.codecool.chatter.model.Connection;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,6 +12,7 @@ public class Server {
 
     private int port;
     private AppController appController;
+    private byte[] buf = new byte[256];
 
     public Server(int port) {
         this.port = port;
@@ -18,19 +21,27 @@ public class Server {
 
     public void startServer() {
         ServerSocket serverSocket = null;
+//        DatagramSocket datagramSocket;
 
         try {
             serverSocket = new ServerSocket(port);
+//            datagramSocket = new DatagramSocket(port);
 
             while (true) {
+
                 Socket socket = serverSocket.accept();
-                Connection connection = new Connection(socket);
+
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+//                datagramSocket.receive(packet);
+
+                Connection connection = new Connection(socket, packet);
                 new EchoThreadServer(connection, appController).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
+                assert serverSocket != null;
                 serverSocket.close();
             } catch (IOException e) {
                 e.printStackTrace();
--- ChatterServer/src/main/java/com/codecool/chatter/model/Connection.java
+++ ChatterServer/src/main/java/com/codecool/chatter/model/Connection.java
@@ -1,22 +1,30 @@
 package com.codecool.chatter.model;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.Socket;
+import java.net.*;
 
 public class Connection {
 
+    private ByteArrayOutputStream bos;
     private ObjectOutputStream objectOutputStream;
     private ObjectInputStream objectInputStream;
     private Socket socket;
+    private DatagramSocket datagramSocket;
+    private DatagramPacket packet;
+    private byte[] buf = new byte[256];
 
 
-    public Connection(Socket socket) throws IOException {
+    public Connection(Socket socket, DatagramPacket packet) throws IOException {
+        this.packet = packet;
         this.socket = socket;
+        datagramSocket = new DatagramSocket();
         objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
         objectOutputStream.flush();
         objectInputStream = new ObjectInputStream(socket.getInputStream());
+        bos = new ByteArrayOutputStream();
     }
 
     public ObjectOutputStream getOutput() {
@@ -33,6 +41,17 @@ public class Connection {
         objectOutputStream.writeObject(objectWrapper);
         objectOutputStream.reset();
         objectOutputStream.flush();
+    }
+
+    public void sendDataByUDPConnection(ObjectWrapper objectWrapper) throws IOException{
+        InetAddress clientAdress = packet.getAddress();
+        int clientPort = packet.getPort();
+        bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(objectWrapper);
+        buf = bos.toByteArray();
+        DatagramPacket response = new DatagramPacket(buf, buf.length, clientAdress, clientPort);
+        datagramSocket.send(response);
     }
 
 
