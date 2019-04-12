d0e06de45da324ea9fe7dfab98821149c4296c89
szymslow12 <szym.slow12@gmail.com>
2019-04-12 16:03:45 +0200

    remove souts and remove commented lines

--- ChatterClient/src/main/java/com/codecool/chatter/controller/Updater.java
+++ ChatterClient/src/main/java/com/codecool/chatter/controller/Updater.java
@@ -47,21 +47,13 @@ public class Updater extends Thread {
         System.out.println("started Updater=" + this.getId() + "...");
         do {
             try {
-//                System.out.println("Checks for access...");
-//                Connection.waitForAccess(connection);
-//                connection.setAvailable(false);
-                System.out.println("Waits for response...");
                 ObjectWrapper data = receiveDataIfAvailable();
-                System.out.println("Checks break conditions... isRunning=" + isRunning + " ObjectWrapper=" + data);
                 if (shouldStop(data)) {
                     break;
                 }
-                System.out.println("Updating View...");
                 Platform.runLater(() -> {
                     updatable.updateView(data, object, eventHandler);
                 });
-//                connection.setAvailable(true);
-                System.out.println("Sleep for 50 milis");
                 sleep(50);
             } catch (IOException e) {
                 e.printStackTrace();
